3cb6584218325797e7cf70af84b929ec4dda3314
lordgreggreg <lordgreggreg@a2871d06-efe8-11dd-a8e2-f7cb3c35fcff>
2009-03-05 05:05:38 +0000

    wrong uuid

--- branches/LGG/GridProxyPlugins/PlugComb/ClientDetectForm1.Designer.cs
+++ branches/LGG/GridProxyPlugins/PlugComb/ClientDetectForm1.Designer.cs
@@ -38,6 +38,7 @@
             this.panel8 = new System.Windows.Forms.Panel();
             this.listBox2 = new System.Windows.Forms.ListBox();
             this.panel7 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
             this.textBox2display = new System.Windows.Forms.TextBox();
             this.textBox1uuid = new System.Windows.Forms.TextBox();
             this.label3 = new System.Windows.Forms.Label();
@@ -47,7 +48,6 @@
             this.panel3 = new System.Windows.Forms.Panel();
             this.label1 = new System.Windows.Forms.Label();
             this.checkBox1 = new System.Windows.Forms.CheckBox();
-            this.button1 = new System.Windows.Forms.Button();
             this.panel2.SuspendLayout();
             this.panel4.SuspendLayout();
             this.panel5.SuspendLayout();
@@ -164,6 +164,16 @@
             this.panel7.TabIndex = 0;
             this.panel7.Paint += new System.Windows.Forms.PaintEventHandler(this.panel7_Paint);
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(215, 71);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(159, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Save";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
             // textBox2display
             // 
             this.textBox2display.BackColor = System.Drawing.Color.Black;
@@ -258,16 +268,6 @@
             this.checkBox1.Text = "Display Next To Av Names";
             this.checkBox1.UseVisualStyleBackColor = true;
             this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(215, 71);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(159, 23);
-            this.button1.TabIndex = 4;
-            this.button1.Text = "Save";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // ClientDetectForm1
             // 
