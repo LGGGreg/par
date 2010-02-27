322d25998e04afca7ab5b303c6d3234dfd2bd67c
lordgreggreg <lordgreggreg@a2871d06-efe8-11dd-a8e2-f7cb3c35fcff>
2010-02-27 04:12:28 +0000

    * made the sit plugins work * removed irc library * added client detection plugin

--- trunk/GridProxyPlugins/PlugComb/ClientDetectForm1.cs
+++ trunk/GridProxyPlugins/PlugComb/ClientDetectForm1.cs
@@ -25,16 +25,40 @@ namespace PubComb
         }
         private void loadDefaults()
         {
-            cl.uid2name.Add(new UUID("c252d89d-6f7c-7d90-f430-d140d2e3fbbe"), "VLife");
-            cl.uid2name.Add(new UUID("adcbe893-7643-fd12-f61c-0b39717e2e32"), "tyk3n");
-            cl.uid2name.Add(new UUID("f3fd74a6-fee7-4b2f-93ae-ddcb5991da04"), "PSL");
-            cl.uid2name.Add(new UUID("77662f23-c77a-9b4d-5558-26b757b2144c"), "PSL");
-            cl.uid2name.Add(new UUID("5aa5c70d-d787-571b-0495-4fc1bdef1500"), "LGG");
-            cl.uid2name.Add(new UUID("8183e823-c443-2142-6eb6-2ab763d4f81c"), "Day Oh");
-            cl.uid2name.Add(new UUID("0f6723d2-5b23-6b58-08ab-308112b33786"), "Cryo");
-            cl.uid2name.Add(new UUID("f5a48821-9a98-d09e-8d6a-50cc08ba9a47"), "NeilLife");
             cl.uid2name.Add(new UUID("0bcd5f5d-a4ce-9ea4-f9e8-15132653b3d8"), "MoyMix");
+            cl.uid2name.Add(new UUID("0f6723d2-5b23-6b58-08ab-308112b33786"), "CryoLife");
+            cl.uid2name.Add(new UUID("11ad2452-ce54-8d65-7c23-05589b59f516"), "VerticalLife");
+            cl.uid2name.Add(new UUID("1c29480c-c608-df87-28bb-964fb64c5366"), "Gemini");
+            cl.uid2name.Add(new UUID("2a9a406c-f448-68f2-4e38-878f8c46c190"), "Meerkat");
+            cl.uid2name.Add(new UUID("2c9c1e0b-e5d1-263e-16b1-7fc6d169f3d6"), "PhoxSL");
+            cl.uid2name.Add(new UUID("3ab7e2fa-9572-ef36-1a30-d855dbea4f92"), "VerticalLife");
+            cl.uid2name.Add(new UUID("3da8a69a-58ca-023f-2161-57f2ab3b5702"), "Operator");
+            cl.uid2name.Add(new UUID("4da16427-d81e-e816-f346-aaf4741b8056"), "iLife");
+            cl.uid2name.Add(new UUID("4e8dcf80-336b-b1d8-ef3e-08dacf015a0f"), "Sapphire");
+            cl.uid2name.Add(new UUID("5262d71a-88f7-ef40-3b15-00ea148ab4b5"), "Gemini.Bot");
+            cl.uid2name.Add(new UUID("58a8b7ec-1455-7162-5d96-d3c3ead2ed71"), "VerticalLife");
+            cl.uid2name.Add(new UUID("5aa5c70d-d787-571b-0495-4fc1bdef1500"), "LGGproxy");
+            cl.uid2name.Add(new UUID("77662f23-c77a-9b4d-5558-26b757b2144c"), "PSL");
+            cl.uid2name.Add(new UUID("7c4d47a3-0c51-04d1-fa47-e4f3ac12f59b"), "CryoLife");
+            cl.uid2name.Add(new UUID("8183e823-c443-2142-6eb6-2ab763d4f81c"), "DayOhproxy");
+            cl.uid2name.Add(new UUID("81b3e921-ee31-aa57-ff9b-ec1f28e41da1"), "Infinity");
+            cl.uid2name.Add(new UUID("841ef25b-3b90-caf9-ea3d-5649e755db65"), "VerticalLife");
+            cl.uid2name.Add(new UUID("872c0005-3095-0967-866d-11cd71115c22"), "Copybotter");
+            cl.uid2name.Add(new UUID("9422e9d7-7b11-83e4-6262-4a8db4716a3b"), "BetaLife");
+            cl.uid2name.Add(new UUID("adcbe893-7643-fd12-f61c-0b39717e2e32"), "tyk3n");
+            cl.uid2name.Add(new UUID("b6820989-bf42-ff59-ddde-fd3fd3a74fe4"), "Meerkat");
+            cl.uid2name.Add(new UUID("c252d89d-6f7c-7d90-f430-d140d2e3fbbe"), "VLife");
+            cl.uid2name.Add(new UUID("c5b570ca-bb7e-3c81-afd1-f62646b20014"), "KungFu");
+            cl.uid2name.Add(new UUID("ccb509cf-cc69-e569-38f1-5086c687afd1"), "Ruby");
             cl.uid2name.Add(new UUID("ccda2b3b-e72c-a112-e126-fee238b67218"), "Emerald");
+            cl.uid2name.Add(new UUID("d3eb4a5f-aec5-4bcb-b007-cce9efe89d37"), "rivlife");
+            cl.uid2name.Add(new UUID("e52d21f7-3c8b-819f-a3db-65c432295dac"), "CryoLife");
+            cl.uid2name.Add(new UUID("e734563e-1c31-2a35-3ed5-8552c807439f"), "VerticalLife");
+            cl.uid2name.Add(new UUID("f12457b5-762e-52a7-efad-8f17f3b022ee"), "Anti-Life");
+            cl.uid2name.Add(new UUID("f3fd74a6-fee7-4b2f-93ae-ddcb5991da04"), "PSL");
+            cl.uid2name.Add(new UUID("f5a48821-9a98-d09e-8d6a-50cc08ba9a47"), "NeilLife");
+            cl.uid2name.Add(new UUID("f5feab57-bde5-2074-97af-517290213eaa"), "Onyx");
+            cl.uid2name.Add(new UUID("ffce04ff-5303-4909-a044-d37af7ab0b0e"), "Corgi");
             
         }
         private void Form1_Load(object sender, EventArgs e)
